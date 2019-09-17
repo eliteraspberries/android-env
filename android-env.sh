1274d88ddb524aa96707b3ffb42290a2b5105874
Mansour Moufid <mansourmoufid@gmail.com>
2019-09-17 16:51:05 -0400

    Support NDK versions r19 and later.

--- android-env.sh
+++ android-env.sh
@@ -7,13 +7,13 @@ if test "${ABI}" = "armeabi-v7a"; then
     ARCH="arm"
     FPU="vfpv4"
     MARCH="armv7-a"
-    TARGET="arm-linux-androideabi"
+    TRIPLET="arm-linux-androideabi"
 elif test "${ABI}" = "arm64-v8a"; then
     if test -z "${API}"; then API="21"; fi
     ARCH="arm64"
     FPU="neon"
     MARCH="armv8-a"
-    TARGET="aarch64-linux-android"
+    TRIPLET="aarch64-linux-android"
 else
     exit 1
 fi
@@ -32,40 +32,70 @@ if test -z "${ANDROIDNDK}"; then
     ANDROIDNDK="${ANDROIDSDK}/ndk-bundle"
 fi
 if ! test -d "${ANDROIDNDK}"; then exit 1; fi
-SYSROOT="${ANDROIDNDK}/sysroot"
-PREFIX="${TARGET}-${API}-toolchain"
-if ! test -d ${PREFIX}; then
-    ${ANDROIDNDK}/build/tools/make_standalone_toolchain.py \
-        --arch ${ARCH} \
-        --api ${API} \
-        --install-dir=${PREFIX}
+CFLAGS=""
+CPPFLAGS=""
+LDFLAGS=""
+NDKVERSION="$( \
+    awk -F'[= ]' '{if ($1 == "Pkg.Revision") print $NF}' \
+    < ${ANDROIDNDK}/source.properties \
+)"
+if test ${NDKVERSION%%.*} -lt 19; then
+    SYSROOT="${ANDROIDNDK}/sysroot"
+    TARGET="${TRIPLET}"
+    TOOLCHAIN="${TARGET}-${API}-toolchain"
+    if ! test -d ${TOOLCHAIN}; then
+        ${ANDROIDNDK}/build/tools/make_standalone_toolchain.py \
+            --arch ${ARCH} \
+            --api ${API} \
+            --install-dir=${TOOLCHAIN}
+    fi
+    TOOLCHAIN="$(cd ${TOOLCHAIN} && pwd)"
+    AR="${TARGET}-ar"
+    AS="${TARGET}-clang"
+    LD="${TARGET}-ld"
+    RANLIB="${TARGET}-ranlib"
+else
+    HOST="$(echo $(uname -s)-$(uname -m) | tr '[A-Z]' '[a-z]')"
+    TOOLCHAIN="${ANDROIDNDK}/toolchains/llvm/prebuilt/${HOST}"
+    SYSROOT="${TOOLCHAIN}/sysroot"
+    case "${ABI}" in
+        "armeabi-v7a")
+            TARGET="armv7a-linux-androideabi${API}"
+            ;;
+        "arm64-v8a")
+            TARGET="aarch64-linux-android${API}"
+            ;;
+        *)
+            ;;
+    esac
+    AR="llvm-ar"
+    AS="llvm-as"
+    LD="ld.lld"
+    RANLIB="llvm-ranlib"
+    LDFLAGS="-L${SYSROOT}/usr/lib/${TRIPLET} ${LDFLAGS}"
+    LDFLAGS="-L${SYSROOT}/usr/lib/${TRIPLET}/${API} ${LDFLAGS}"
 fi
-PREFIX="$(cd ${PREFIX} && pwd)"
-AR="${TARGET}-ar"
-AS="${TARGET}-clang"
 CC="${TARGET}-clang"
 CPP="${CC} -E"
 CXX="${TARGET}-clang++"
-LD="${TARGET}-ld"
-RANLIB="${TARGET}-ranlib"
-CFLAGS=""
 CFLAGS="--sysroot=${SYSROOT} ${CFLAGS}"
+CFLAGS="--target=${TARGET} ${CFLAGS}"
 CFLAGS="-fpic ${CFLAGS}"
 CFLAGS="-march=${MARCH} ${CFLAGS}"
 CFLAGS="-mfloat-abi=${FLOATABI} ${CFLAGS}"
 CFLAGS="-mfpu=${FPU} ${CFLAGS}"
-CPPFLAGS=""
 CPPFLAGS="-D__ANDROID_API__=${API} ${CPPFLAGS}"
 CPPFLAGS="-isysroot ${SYSROOT} ${CPPFLAGS}"
-CPPFLAGS="-isystem ${SYSROOT}/usr/include/${TARGET} ${CPPFLAGS}"
+CPPFLAGS="-isystem ${SYSROOT}/usr/include/${TRIPLET} ${CPPFLAGS}"
 CXXFLAGS="${CFLAGS}"
 ANDROIDLDSYSROOT="${ANDROIDNDK}/platforms/android-${API}/arch-${ARCH}"
-LDFLAGS=""
 LDFLAGS="--sysroot=${ANDROIDLDSYSROOT} ${LDFLAGS}"
-PATH="${PREFIX}/bin:${PATH}"
-export ANDROIDABI="${ABI}"
+PATH="${TOOLCHAIN}/bin:${PATH}"
+PATH="${TOOLCHAIN}/${TRIPLET}/bin:${PATH}"
+export ABI
 export ANDROIDLDSYSROOT
 export ANDROIDSYSROOT="${SYSROOT}"
+export TARGET
 export AR
 export AS
 export CC
